3fc6bef859a2a460b278c13e72c2492c421ab7b5
marciosan <mrc.snts@gmail.com>
2016-11-14 17:28:45 +0000

    first version of ParserFacade

--- src/main/java/tecnico/ssof/project/ParserFacade.java
+++ src/main/java/tecnico/ssof/project/ParserFacade.java
@@ -1,8 +1,13 @@
 package tecnico.ssof.project;
 
-import java.io.File;
+import tecnico.ssof.project.parser.*;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 
-import org.antlr.v4.runtime.RuleContext;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 public class ParserFacade {
 
@@ -14,16 +19,34 @@ public class ParserFacade {
 	
 	/// Parse the given file
 	///
+	/// @param file - file to parse
 	/// @return: the root RuleContext (first rule of the grammar)
-	public RuleContext parse(File file) {
+	public PHPParser.HtmlDocumentContext parse(File file) throws IOException {
 		
-		// TODO
-		return null;
+        String code = readFile(file, Charset.forName(ENCODING));
+        
+        // create lexer and give it the code
+        PHPLexer lexer = new PHPLexer(new ANTLRInputStream(code));
+        
+        // get tokens
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        
+        // create parser and give it the tokens
+        PHPParser parser = new PHPParser(tokens);
+
+        // parse starting at rule htmlDocument
+        return parser.htmlDocument();
 	}
 	
-	private String readFile(File file) {
+	/// get String from File
+	///
+	/// @param file - a file
+	/// @param encoding - encoding from file
+	/// @return: the content of the file as a String
+	private static String readFile(File file, Charset encoding) throws IOException {
 		
-		// TODO
-		return null;
-	}
+        byte[] encoded = Files.readAllBytes(file.toPath());
+        
+        return new String(encoded, encoding);
+    }
 }
